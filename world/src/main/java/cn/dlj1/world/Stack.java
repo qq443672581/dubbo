40b053a35c72299139817da539f1bf06b827237d
fivewords <443672581@qq.com>
2019-09-21 20:28:45 +0800

    init

--- world/src/main/java/cn/dlj1/world/Stack.java
+++ world/src/main/java/cn/dlj1/world/Stack.java
@@ -2,5 +2,16 @@ package cn.dlj1.world;
 
 public class Stack {
 
+    public static void main(String[] args) {
+        byte b = -1;
+        char c = 'a';
+        int i = 1;
+        short s = 2;
+        long l = 3;
+        float f = 5.5f;
+        double d = 1.1f;
+
+    }
+
 
 }
