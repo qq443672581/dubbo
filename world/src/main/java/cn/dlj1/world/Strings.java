eeae7f97e0bdc0f888cf75ed26e4583edece88ef
fivewords <443672581@qq.com>
2019-09-28 18:12:23 +0800

    init

---
+++ world/src/main/java/cn/dlj1/world/Strings.java
@@ -0,0 +1,23 @@
+package cn.dlj1.world;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class Strings {
+
+    public static void main(String[] args) {
+//        int size = 15000 * 1;
+//        String[] arr = new String[size];
+//        for (int i = 0; i < size; i++) {
+//            arr[i] = new String(UUID.randomUUID().toString());
+//        }
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
